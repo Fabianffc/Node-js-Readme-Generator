a966289c37eb93b4f744bb9395bb5166f74f74a8
Fabian <fabianffc@gmail.com>
2020-12-04 18:50:43 -0600

    questions for readme

--- index.js
+++ index.js
@@ -5,20 +5,43 @@ inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your username?',
+      name: 'title',
+      message: 'What is your project title?',
     },
     {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+      type: 'input',
+      message: 'Give a short description of your project?',
+      name: 'description',
     },
     {
-      type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'telekinesis'],
+      type: 'input',
+      message: 'How to install?',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'How to use the application?',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Do you have any cotributors?',
+      name: 'contributors',
+    },
+    {
+      type: 'input',
+      message: 'How to test the app?',
+      name: 'test',
+    },
+    {
+      type: 'input',
+      message: 'What is your github username?',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'What is your email address?',
+      name: 'email',
     },
   ])
   .then((data) => {
